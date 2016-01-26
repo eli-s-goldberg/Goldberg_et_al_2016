4aebc2f0ca59c2c09641c05b1e8b0a9411830e05
PandaStabber <elisgoldberg@gmail.com>
2016-01-26 13:35:21 +0100

    Initial commit

---
+++ helperFunctions.py
@@ -0,0 +1,404 @@
+def binaryRPClassAssign(row):
+    if (row.ObsRPShape == 'EXP' or row.ObsRPShape == 'HE'):
+        a = 0
+    else:
+        a = 1
+    # print a
+    return a
+
+def binaryRPClassAssignLabels(row):
+    if (row.ObsRPShape == "EXP" or row.ObsRPShape == "HE"):
+        a =  "exponential"
+    else:
+        a = "nonexponential"
+    # print a
+    return a
+
+def dimAspectRatioAssign(row):
+    return float(row.PartDiam / row.CollecDiam)
+
+
+def dimPecletNumAssign(row):
+    import math
+    bolzConstant = float(1.3806504 * 10 ** -23)
+    tempK = 25 + 273.15
+    diffusionCoef = float(bolzConstant * tempK / (3 * math.pi * 8.94e-4 * row.PartDiam))
+    return float(row.Darcy * row.CollecDiam / diffusionCoef)
+
+
+def gravitationalNumber(row):
+    import math
+    p_radius = float((row.PartDiam) / 2)
+    bolzConstant = float(1.3806504 * 10 ** -23)
+    tempK = 25 + 273.15
+    return float((4 / 3) * (math.pi * p_radius ** 4) * (row.PartDensity - 1000) * 9.81 / (bolzConstant * tempK))
+
+
+def attractionNumber(row):
+    import math
+    p_radius = float((row.PartDiam) / 2)
+    denominator = 12 * math.pi * p_radius ** 2 * row.Darcy
+    return float(row.Hamaker / denominator)
+
+
+def gravityNumber(row):
+    import math
+    A = float(-3.7188)
+    B = float(578.919)
+    C = float(-137.546)
+    tempK = row.tempKelvin
+
+    absVisc = float(math.exp(A + (B / (C + tempK))))
+    p_radius = float((row.PartDiam) / 2)
+    numerator = (2 * p_radius ** 2) * (row.PartDensity - 1000) * 9.81
+    demoninator = 8 * absVisc * row.Darcy
+    return float(numerator / demoninator)
+
+
+def debyeLength(row):
+    permFreeSpace = float(8.854 * 10 ** -12)
+    bolzConstant = float(1.3806504 * 10 ** -23)
+    elecCharge = float(1.602176466 * 10 ** -19)
+    numAvagadro = float(6.02 * 10 ** 23)
+    if (row['SaltType'] == 3):
+        ionStr1 = float(1 * 10 ** (float(row['pH']) - 14))
+        ionStr2 = float(1 * 10 ** (-1 * float(row['pH'])))
+        ZiCi = float(1 ** 2 * ionStr1 + 1 ** 2 * ionStr2)
+        return float((1 / (float((numAvagadro * elecCharge ** 2 / (
+            permFreeSpace * float(row['relPermValue']) * bolzConstant * float(row['tempKelvin'])) * ZiCi) ** 0.5))))
+    elif (row['SaltType'] == 1):
+        ZiCi = float(2 ** 2 * float(row['IonStr']) + 1 ** 2 * 2 * float(row['IonStr']))
+        return float((1 / (float((numAvagadro * elecCharge ** 2 / (
+            permFreeSpace * float(row['relPermValue']) * bolzConstant * float(row['tempKelvin'])) * ZiCi) ** 0.5))))
+    elif (row['IonStr'] == 0):
+        return 1000e-9  # about 1um
+    else:
+        ZiCi = float(1 ** 2 * float(row['IonStr']) + 1 ** 2 * float(row['IonStr']))
+        return float((1 / (float((numAvagadro * elecCharge ** 2 / (
+            permFreeSpace * float(row['relPermValue']) * bolzConstant * float(row['tempKelvin'])) * ZiCi) ** 0.5))))
+
+
+def massFlow(row):
+    import math
+    L = row.colLength
+    W = row.colWidth
+    A = float(math.pi / 4 * row.colWidth ** 2)
+    p = row.Poros
+    PVs = row.PvIn
+    return float(L * W * A * p * PVs)
+
+
+def electrokinetic1(row):
+    v = 7.83e-9  # dialectric constant of water at 25C in coulombs/volt/m
+    a = row.PartDiam / 2  # particle radius
+    Zp = row.PartZeta / 1e3  # particle zeta potential
+    Zc = row.CollecZeta / 1e3  # collector zeta potential
+    k = float(1.3806504 * 10 ** -23)  # boltzmann constant
+    T = row.tempKelvin  # temperature
+
+    return float(v * a * (Zp ** 2 + Zc ** 2) / (4 * k * T))
+
+
+def electrokinetic2(row):
+    v = 7.83e-9  # dialectric constant of water at 25C in coulombs/volt/m
+    a = row.PartDiam / 2  # particle radius
+    Zp = row.PartZeta / 1e3  # particle zeta potential
+    Zc = row.CollecZeta / 1e3  # collector zeta potential
+    k = float(1.3806504 * 10 ** -23)  # boltzmann constant
+    T = row.tempKelvin  # temperature
+    numerator = 2 * (Zp / Zc)
+    denominator = 1 + (Zp / Zc) ** 2
+    return float(numerator / denominator)
+
+
+def relPermittivity(row):
+    if (row['NMId'] == 0):  # C60
+        relPermVal = 4.4
+    elif (row['NMId'] == 1):  # TiO2
+        relPermVal = 110
+    elif (row['NMId'] == 2):  # ZnO
+        relPermVal = 2
+    elif (row['NMId'] == 3):  # CuO
+        relPermVal = 18.1
+    elif (row['NMId'] == 4):  # MWCNTs
+        relPermVal = 1328
+    elif (row['NMId'] == 5):  # Ag
+        relPermVal = 2.65
+    elif (row['NMId'] == 6):  # CeO2
+        relPermVal = 26
+    elif (row['NMId'] == 7):  # Iron Oxide
+        relPermVal = 14.2
+        # elif (row['NMId'] == 8):  # SiO2 # removed because no SiO2 with hamakers
+        #     relPermVal = 3.9
+    elif (row['NMId'] == 8):  # nHAP
+        relPermVal = 15.4  # originally reported at 7, but found it at 15.4 at 100Hz in Preparation and dielectric
+        # property of sintered monoclinic hydroxyapatite
+    # Toshiyuki Ikoma, Atsushi Yamazaki, Satoshi Nakamura, Masaru Akao
+    elif (row['NMId'] == 9):  # nBiochar Dielectric properties and microwave heating of oil palm biomass and biochar
+        relPermVal = 2.9
+    elif (row[
+              'NMId'] == 10):  # CdSe http://scholar.lib.vt.edu/theses/available/etd-04262005-181042/unrestricted
+        # /Ch2Theory.pdf
+        relPermVal = 10
+    else:
+        relPermVal = 10
+    return float(relPermVal)
+
+
+def changEta0(row):
+    N_Dl = row.N_Dl
+    N_Z1 = row.N_Z1
+    N_Z2 = row.N_Z2
+    N_Lo = row.N_Lo
+    N_as = row.N_as
+    N_r = row.N_r
+    N_Pe = row.N_Pe
+    N_g = row.N_g
+
+    return float(0.024 * N_Dl ** (0.969) * N_Z1 ** (-0.423) * N_Z2 ** (2.880) * N_Lo ** 1.5 + \
+                 3.176 * N_as ** (0.333) * N_r ** (-0.081) * N_Pe ** (-0.715) * N_Lo ** (2.687) + \
+                 0.222 * N_as * N_r ** (3.041) * N_Pe ** (-0.514) * N_Lo ** (0.125) + \
+                 N_r ** (-0.24) * N_g ** (1.11) * N_Lo)
+
+
+def londonForce(row):
+    k = float(1.3806504 * 10 ** -23)
+    T = row.tempKelvin
+    A = row.Hamaker
+    return float(A / (6 * k * T))
+
+
+def zetaRatioKnockout(row):
+    if row.N_z < 0:
+        row.N_z = 0
+    else:
+        row.N_z = row.N_z
+    return (row.N_z)
+
+
+def heldout_score(clf, X_test, y_test):
+    """compute deviance scores on ``X_test`` and ``y_test``. """
+    clf.fit(X_test, y_test)
+    score = np.zeros((n_estimators,), dtype=np.float64)
+    for i, y_pred in enumerate(clf.staged_decision_function(X_test)):
+        score[i] = clf.loss_(y_test, y_pred)
+    return score
+
+
+def porosityHappel(row):
+    import math
+    p = float(row.Poros)
+    gam = (1 - p) ** (.333333333)
+    numerator = 2 * (1 - gam ** 5)
+    denominator = 2 - 3 * gam + 3 * gam ** 5 - 2 * gam ** 6
+    return float(numerator / denominator)
+
+
+def debyeNumber(row):
+    D = row.D_l
+    dp = row.PartDiam
+    return float(dp / D)
+
+def rulesGradientBoost(clf, features, labels, node_index=0):
+
+    """Structure of rules in a fit decision tree classifier
+
+    Parameters
+    ----------
+    clf : DecisionTreeClassifier
+        A tree that has already been fit.
+
+    features, labels : lists of str
+        The names of the features and labels, respectively.
+
+    """
+    import ast
+    from decimal import Decimal
+
+    node = {}
+    if clf.tree_.children_left[node_index] == -1:  # indicates leaf
+        count_labels = zip(clf.tree_.value[node_index, 0], labels)
+        samples = clf.tree_.n_node_samples[node_index]
+        impurity = round(clf.tree_.impurity[node_index],2)
+
+        node['name'] = ', '.join(('{} '.format(count, label) for count, label in count_labels))
+        if  Decimal(node['name']) < 0 :
+            print ast.literal_eval(node['name'])
+
+            node['name'] = '{}, nonexponential'.format(round(Decimal(node['name']),2)) #'nonexponential'
+
+        else:
+            # node['name'] = 'exponential'
+            node['name'] = '{}, exponential'.format(round(Decimal(node['name']),2))
+        node['samples'] = samples
+        node['impurity'] = "{}".format(impurity)
+        # node['samples'] = ', '.join('{}'.format(samples))
+
+
+    else:
+        feature = features[clf.tree_.feature[node_index]]
+        threshold = '%.3E' % Decimal(clf.tree_.threshold[node_index])
+        samples = clf.tree_.n_node_samples[node_index]
+        # impurity = clf.tree_.impurity[node_index]
+
+
+        node['name'] = "{} >= {}".format(feature, threshold)
+        node['samples'] = '{}'.format(samples)
+        # node['impurity'] = '{}%'.format(impurity)
+
+
+        left_index = clf.tree_.children_left[node_index]
+        right_index = clf.tree_.children_right[node_index]
+        node['children'] = [rules(clf, features, labels, right_index),
+                            rules(clf, features, labels, left_index)]
+    # print node
+    return node
+
+def rules(clf, features, labels, node_index=0):
+
+    """Structure of rules in a fit decision tree classifier
+
+    Parameters
+    ----------
+    clf : DecisionTreeClassifier
+        A tree that has already been fit.
+
+    features, labels : lists of str
+        The names of the features and labels, respectively.
+
+    """
+    import ast
+    from decimal import Decimal
+
+    node = {}
+
+    if clf.tree_.children_left[node_index] == -1:  # indicates leaf
+        count_labels = zip(clf.tree_.value[node_index, 0], labels)
+        samplesTerminal = clf.tree_.n_node_samples[node_index]
+
+        impurity = round(clf.tree_.impurity[node_index],3)
+
+        node['name'] = ', '.join(('{} {}'.format('%.2f' % Decimal(count), label)
+                                  for count, label in count_labels))
+
+        node['impurity'] = "{}".format(impurity)
+        node['samples'] = "{}".format(samplesTerminal)
+        # print node['samples']
+
+    else:
+        samples = clf.tree_.n_node_samples[node_index]
+        feature = features[clf.tree_.feature[node_index]]
+        threshold = '%.3E' % Decimal(clf.tree_.threshold[node_index])
+
+        node['name'] = "{} >= {}".format(feature, threshold)
+        node['samples'] = '{}'.format(samples)
+
+
+        left_index = clf.tree_.children_left[node_index]
+        right_index = clf.tree_.children_right[node_index]
+        node['children'] = [rules(clf, features, labels, right_index),
+                            rules(clf, features, labels, left_index)]
+    return node
+
+def NMIDClassAssign(row):
+    if row.NMId == 0:
+        a = "C60"
+    elif row.NMId == 1:
+        a = "TiO2"
+    elif row.NMId == 2:
+        a = "ZnO"
+    elif row.NMId == 3:
+        a = "CuO"
+    elif row.NMId == 4:
+        a = "MWCNT"
+    elif row.NMId == 5:
+        a = "Ag"
+    elif row.NMId == 6:
+        a = "CeO"
+    elif row.NMId == 7:
+        a = "FeOx"
+    elif row.NMId == 8:
+        a = "HAP"
+    elif row.NMId == 9:
+        a = "Biochar"
+    elif row.NMId == 10:
+        a = "QD"
+    # print a
+    return a
+
+def SaltClassAssign(row):
+    if row.SaltType == 0:
+        a = "NaCl"
+    elif row.SaltType == 1:
+        a = "CaCl2"
+    elif row.SaltType == 2:
+        a = "KCl"
+    elif row.SaltType == 3:
+        a = "None"
+    elif row.SaltType == 4:
+        a = "KNO3"
+    elif row.SaltType == 5:
+        a = "NaHCO3"
+    return a
+
+def CoatingClassAssign(row):
+    if row.Coating == 0:
+        a = "None"
+    elif row.Coating == 1:
+        a = "IronOxide"
+    elif row.Coating == 2:
+        a = "FeOOH"
+    return a
+
+def TypeNOMClassAssign(row):
+    if row.TypeNOM == 0:
+        a = "None"
+    elif row.TypeNOM == 1:
+        a = "SRHA"
+    elif row.TypeNOM == 2:
+        a = "Alg"
+    elif row.TypeNOM == 3:
+        a = "TRIZMA"
+    elif row.TypeNOM == 4:
+        a = "FA"
+    elif row.TypeNOM == 5:
+        a = "HA"
+    elif row.TypeNOM == 6:
+        a = "Citric"
+    elif row.TypeNOM == 7:
+        a = "Oxalic"
+    elif row.TypeNOM == 8:
+        a = "Formic"
+    # print a
+    return a
+
+
+def one_hot_dataframe(data, cols, replace=False):
+
+
+    """
+    Small script that shows hot to do one hot encoding
+    of categorical columns in a pandas DataFrame.
+    See:
+    http://scikit-learn.org/dev/modules/generated/sklearn.preprocessing.OneHotEncoder.html#sklearn.preprocessing.OneHotEncoder
+    http://scikit-learn.org/dev/modules/generated/sklearn.feature_extraction.DictVectorizer.html
+
+
+    https://gist.github.com/kljensen/5452382
+
+    Takes a dataframe and a list of columns that need to be encoded.
+        Returns a 3-tuple comprising the data, the vectorized data,
+        and the fitted vectorizor.
+    """
+    import pandas
+    import numpy
+    from sklearn.feature_extraction import DictVectorizer
+    vec = DictVectorizer()
+    mkdict = lambda row: dict((col, row[col]) for col in cols)
+    vecData = pandas.DataFrame(vec.fit_transform(data[cols].apply(mkdict, axis=1)).toarray())
+    vecData.columns = vec.get_feature_names()
+    vecData.index = data.index
+    if replace is True:
+        data = data.drop(cols, axis=1)
+        data = data.join(vecData)
+    return (data, vecData, vec)
---
+++ optimalTree.py
@@ -0,0 +1,293 @@
+# To install requirements pip install -r requirements.txt
+'''
+This file does some interesting things. It imports the developed database, converts it to dimensionless parameters, and
+then trains a decision tree based on those features. Here it is important to remember that we're not evaluating the
+generic performance of the decision tree to predict the data outcome. We're using the decision tree as a method to
+quantitatively investigate and breakdown the data... to see if we can disentangle the relationship between physicochem-
+ical parameters and the retention behavior of nanomaterials.
+
+Note that, becuase there are elements of stochasticity in the decision tree growing process, it can be difficult to
+obtain the optimal decision tree (n.b. for most cases, there is never an optimal decision tree). Here, we cannot
+guarentee optimality, but we can iteratively investigate the results of the decision tree and pick the best one.
+
+Here we employ 5000 decision tree runs and then
+'''
+import multiprocessing
+
+multiprocessing.cpu_count()
+from helperFunctions import *
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import os
+from sklearn import metrics
+from sklearn import grid_search
+
+from sklearn.externals.six import StringIO
+from sklearn import tree  # old scikit learn 0.16.1
+import pydot
+import json
+from sklearn.metrics import classification_report
+
+# Set path
+path = os.path.join('./')
+
+# Set model iterations, cross folds, and randomness
+iterations = 50  # Number of model iterations
+kNum = 5  # Number of cross folds
+
+# Set stack names for
+importNames = []
+for i in range(0, iterations):
+    importNames.append('enmTransportData.xlsx')
+
+# initialize containers for speed
+rfecvGridScoresAll = pd.DataFrame()
+bestParamsResults = pd.DataFrame()
+dfGoodBadDataTrain = pd.DataFrame()
+optimumLengthAll = pd.DataFrame()
+outputScoresCV = pd.DataFrame()
+testIndexTrack = pd.DataFrame()
+trainIndexTrack = pd.DataFrame()
+mseScoresTrack = pd.DataFrame()
+featureImportanceStore = pd.DataFrame()
+r2scoreHoldoutTrack = pd.DataFrame()
+CVR2scoreTrack = pd.DataFrame()
+rfecvGridScoresAll = pd.DataFrame()
+optimumLengthAll = pd.DataFrame()
+nameListAll = pd.DataFrame()
+nameListAll = pd.DataFrame()
+y_pred2Track = pd.DataFrame()
+y_holdoutTrack = pd.DataFrame()
+rfecvGridScoresWithRegNameTrack = pd.DataFrame()
+contourTrack = pd.DataFrame()
+xxTrack = pd.DataFrame()
+yyTrack = pd.DataFrame()
+y_randTest = pd.DataFrame()
+f1_binary_average_score_track = []
+f1_report = pd.DataFrame()
+
+if __name__ == '__main__':  # wrap inside to prevent parallelize errors on windows.
+    run = 0  # set initial run ID
+
+    for name in importNames:  # Loop through all model iterations by looping through database names
+
+        run = run + 1  # Allocate current run ID
+
+        print run  # Print for convenience
+
+        # import database as a function of path and iterated database name
+        alphaDataset = pd.read_excel(os.path.join(path, 'transport_database', name))
+
+        # Identify columns that are not needed for the assessment and drop
+        dropColumnList = [
+            'ProfileID',
+            'ColumnLWRatio',
+            'mbEffluent',
+            'mbRetained',
+            'mbEffluent_norm',
+            'mbRetained_norm',
+            'Dispersivity',
+            'Notes1',
+            'Notes2',
+            'Notes3',
+            'Notes4',
+            'Notes5']
+
+        alphaDataset = alphaDataset.drop(dropColumnList, 1)
+
+        # remove any row without data (NA)
+        alphaDataset = alphaDataset.dropna()
+
+        # save the dataset for later inspection and use as refinedDataset
+        alphaDataset.to_csv('refinedDataset.csv')
+
+        # copy the refined dataset to a new variable.
+        trainingData = alphaDataset.copy(deep=True)
+
+        # drop the classification from the training data (that wouldn't be fun:)
+        trainingData = trainingData.drop(['ObsRPShape'], 1)
+
+        # Set the target data, copy into a new database and binarize inputs to exponential or nonexponential.
+        targetData = pd.DataFrame(alphaDataset.ObsRPShape, columns=['ObsRPShape'])
+        targetData = targetData.apply(binaryRPClassAssign, axis=1)
+
+        # categorical feature evaluation: Step 1 create containers for feature names and dataframe for uniques.
+        trainingCatFeatureNames = []
+        trainingFeatureUniques = pd.DataFrame()
+
+        # categorical feature evaluation: Step 2 copy features into a seperate database so you don't mess up the first
+        trainDataFeatureInspect = trainingData.copy(deep=True)
+
+        # set an if statment to check what the categorical features are.
+        if name == 'enmTransportData.xlsx':
+            trainingCatFeatureNames = [
+                'NMId',
+                'SaltType',
+                'Coating',
+                'TypeNOM'
+            ]
+        else:  # set your own if these are not they
+            trainingCatFeatureNames = []
+
+        # categorical feature evaluation: Step 3 loop through names, pull out and store uniques and factorize
+        for features in trainingCatFeatureNames:
+            tempdf = pd.DataFrame(trainDataFeatureInspect[features].factorize()[1], columns=[features])
+            trainingFeatureUniques = pd.concat([trainingFeatureUniques, tempdf], axis=1)
+            trainDataFeatureInspect[features] = trainDataFeatureInspect[features].factorize()[0]  # factorized data
+
+        # create temporary dataframe and store names. Concat with the uniques in case the database name changes.
+        tempdf2 = pd.DataFrame(columns=[name])
+        trainingFeatureUniques = pd.concat([trainingFeatureUniques, tempdf2], axis=1)
+
+        # print the unique features. This is to inspect the range of features, but also to get their assigned numbers.
+        # print trainingFeatureUniques # Don't bother
+
+        # Once again copy the factorized data from the above loop into the training data.
+        trainingData = trainDataFeatureInspect.copy(deep=True)
+
+        # Apply dimensionless number feature dimension reduction, start by assigning assumed constants (i.e., temp)
+        # Note that this requires some data to be factorized. This will be changed in later versions, but now it
+        # requires that we factorize, then apply and conform to dimensionless paramters, and then reencode to
+        # recover categorical variables.
+        trainingData['tempKelvin'] = 25 + 273.15
+        trainingData['relPermValue'] = trainingData.apply(relPermittivity, axis=1)
+        trainingData['N_r'] = trainingData.apply(dimAspectRatioAssign, axis=1)
+        trainingData['N_a'] = trainingData.apply(attractionNumber, axis=1)
+        trainingData['N_g'] = trainingData.apply(gravityNumber, axis=1)
+        trainingData['N_Pe'] = trainingData.apply(dimPecletNumAssign, axis=1)
+        trainingData['N_Lo'] = trainingData.apply(londonForce, axis=1)
+        trainingData['D_l'] = trainingData.apply(debyeLength, axis=1)
+        trainingData['N_Dl'] = trainingData.apply(debyeNumber, axis=1)
+        trainingData['M_inj'] = trainingData.apply(massFlow, axis=1)
+        trainingData['M_inj'] = 1e6 * trainingData['M_inj']  # in mg
+        trainingData['N_as'] = trainingData.apply(porosityHappel, axis=1)
+        trainingData['N_Z1'] = trainingData.apply(electrokinetic1, axis=1)
+        trainingData['N_Z2'] = trainingData.apply(electrokinetic2, axis=1)
+        trainingData['N_CA'] = trainingData['colLength'] / trainingData['colWidth']
+        trainingData['ConcIn'] = 1e3 * trainingData['ConcIn']  # in mg/L
+        trainingData['ConcHA'] = 1e3 * trainingData['ConcHA']  # in mg/L
+
+        # put back categorical data encodes (see note above)
+        trainingData['NMId'] = trainingData.apply(NMIDClassAssign, axis=1)
+        trainingData['SaltType'] = trainingData.apply(SaltClassAssign, axis=1)
+        trainingData['Coating'] = trainingData.apply(CoatingClassAssign, axis=1)
+        trainingData['TypeNOM'] = trainingData.apply(TypeNOMClassAssign, axis=1)
+
+        # Output a final copy of the training data for later use
+        trainingData.to_csv('./trainingdataAll.csv', index=False, header=True)
+
+        # Drop overlapping features - Combination assessment: temporary
+        trainingData = trainingData.drop(['PublicationTitle', 'relPermValue', 'PartDensity', 'PvIn', 'Poros', 'D_l',
+                                          'tempKelvin', 'colLength', 'colWidth',
+                                          'PartDiam', 'CollecDiam',
+                                          'Darcy', 'IonStr', 'SaltType', 'pH',
+                                          'PartZeta', 'CollecZeta', 'PartIEP', 'Hamaker'
+                                          ], 1)
+
+        # More saving, post feature drop.
+        targetData.to_csv('./targetdata.csv', index=False, header=True)
+        trainingData.to_csv('./trainingdata.csv', index=False, header=True)
+
+        # for use with the decision tree, get the remaining feature names
+        feature_names = list(trainingData.columns.values)
+
+        # encode the categorical variables using a one-hot scheme so they're correctly considered in decision tree
+        ohe_enc = OneHotEncoder()
+        trainingData, _, _ = one_hot_dataframe(trainingData, ['NMId', 'ConcIn', 'Coating', 'TypeNOM'], replace=True)
+
+        # assign the target data as yAll and the training data as XAll. Notice that we train AND test on the same data.
+        # This is not commmon, but we're employing the decision tree for a descriptive evaluation, not it's generic
+        # prediction performance
+        yAll = targetData.as_matrix()
+        XAll = trainingData.as_matrix()
+
+        # initialize the classifier
+        clf = tree.DecisionTreeClassifier()
+
+        # set a grid of parameters to investigate
+        dpgrid = {'max_depth': [3, 4, 5],
+                  'min_samples_leaf': [5, 6, 7, 8, 9, 10],
+                  'max_features': [None, 'sqrt', 'log2'],  # , 'sqrt', 'log2'
+                  'random_state': [None]
+                  # 'class_weight': ['balanced'] # balance messes up the feature cound, use with caution
+                  }
+
+        # investigate the best possible set of parameters using a cross validation loop and the given grid
+        gridSearch = grid_search.GridSearchCV(estimator=clf,
+                                              cv=kNum,
+                                              param_grid=dpgrid,
+                                              n_jobs=-1)
+
+        # call the grid search fit using the data
+        gridSearch.fit(XAll, yAll)
+
+        # store and print the best parameters
+        bestParams = gridSearch.best_params_
+        print bestParams
+
+        # reinitialize and call the classifier with the best parameter
+        clf = tree.DecisionTreeClassifier(**bestParams)
+        clf.fit(XAll, yAll)
+
+        # Evaluate the performance
+        y_pred = clf.predict(XAll)  # predict the outcome by inputting all the data (this gives us the score)
+
+        # calculate the score for the combined class (weighted), and then each class individually
+        f1_binary_average_score = metrics.f1_score(yAll, y_pred, pos_label=None, average='weighted')
+        f1_binary_average_score_exponential = metrics.f1_score(yAll, y_pred, pos_label=0)
+        f1_binary_average_score_nonexponential = metrics.f1_score(yAll, y_pred, pos_label=1)
+
+        # initialize scoring tracking dataframe to store the data
+        f1_track = pd.DataFrame()
+        f1_track['exponential'] = f1_binary_average_score_exponential,
+        f1_track['nonexponential'] = f1_binary_average_score_nonexponential
+        f1_track['average'] = f1_binary_average_score
+        f1_report = f1_report.append(f1_track)
+        f1_binary_average_score_track.append(f1_binary_average_score)
+
+        # Compare the predictions to the truth directly and outut a file to inspect. 
+        y_pred_frame = pd.DataFrame(y_pred, columns=['predicted'])
+        y_truth_frame = pd.DataFrame(yAll, columns=['truth'])
+        comparison = pd.concat([y_pred_frame, y_truth_frame], axis=1)
+        comparison.to_csv('comparison.csv')
+
+        ## The following section creates figures to visualize the decision tree as a PDF and to plot in D3 (java/html)
+        # Feature elimination is not included here, but was included previously. This grabs only the names in the
+        # remaining features.
+        grabWorkingNames = [str(i) for i in list(trainingData)]
+
+        # set the path to save the json representation.
+        json_path = os.path.join(path, 'figures', 'decisionTreeVisualization', 'flare_reports', 'flare' + str(run) + '.json')
+
+        dataTargetNames = ['exponential', 'nonexponential']
+        r = rules(clf, grabWorkingNames, dataTargetNames)
+        with open(json_path, 'w') as f:
+            f.write(json.dumps(r))
+        f.close()
+
+        dot_data = StringIO()
+        tree.export_graphviz(clf, out_file=dot_data,
+                             feature_names=grabWorkingNames, impurity=True, rounded=True,
+                             filled=True, label='all', leaves_parallel=True,
+                             class_names=['exponential', 'nonexponential']
+                             )
+
+        graph = pydot.graph_from_dot_data(dot_data.getvalue())
+        graph.write_pdf(str(path) + '/output/trees/tree' + str(run) + '.pdf')
+        class_reportPath = os.path.join(path, 'figures', 'decisionTreeVisualization', 'class_reports',
+                                        'class_report' + str(run) + '.txt')
+        # if there is an existing file with this name, clear it.
+        file = open(class_reportPath, "w").close()
+
+        # write code custom json because nothing else works.
+        file = open(class_reportPath, "a")
+
+        file.write(classification_report(yAll, y_pred, target_names=['exponential', 'nonexponential']))
+        file.write('\n')
+        file.close()
+
+    ReportSavePath = os.path.join(path, 'figures', 'decisionTreeVisualization', 'DecisiontreeScores' + str(run) + '.csv')
+    f1_report.to_csv(ReportSavePath)
+    f1_report.reset_index(inplace=True)
+    print f1_report.describe
+    print "best performing decision tree index: ", f1_report['average'].argmax()
